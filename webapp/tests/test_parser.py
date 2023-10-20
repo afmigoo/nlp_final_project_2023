dd6bc8ac2521dc7ed6f87adb5e79f10889548a97
Ivan Osorgin <igosorgin@edu.hse.ru>
2023-10-20 14:14:32 +0300

    tests moved

--- webapp/tests/test_parser.py
+++ webapp/tests/test_parser.py
@@ -1,7 +1,7 @@
 import unittest
 
-from ._parser import get_text_trigrams
-from ._data_structures import Token, Trigram
+from pasta_parser._parser import get_text_trigrams
+from pasta_parser._data_structures import Token, Trigram
 
 class TestTrigramParser(unittest.TestCase):
 
@@ -72,6 +72,23 @@ class TestTrigramParser(unittest.TestCase):
             Trigram((token1, token2, token3)),
             Trigram((token2, token3, None)),
             Trigram((token3, None, None))
+        ]
+        result = list(get_text_trigrams(input_text))
+        self.assertListEqual(output, result)
+
+    def test_adp(self):
+        input_text = 'у за под в'
+        token1 = Token((0, 1), 'у', 'у', 'adp')
+        token2 = Token((2, 4), 'за', 'за', 'adp')
+        token3 = Token((5, 8), 'под', 'под', 'adp')
+        token4 = Token((9, 10), 'в', 'в', 'adp')
+        output = [
+            Trigram((None, None, token1)),
+            Trigram((None, token1, token2)),
+            Trigram((token1, token2, token3)),
+            Trigram((token2, token3, token4)),
+            Trigram((token3, token4, None)),
+            Trigram((token4, None, None)),
         ]
         result = list(get_text_trigrams(input_text))
         self.assertListEqual(output, result)
